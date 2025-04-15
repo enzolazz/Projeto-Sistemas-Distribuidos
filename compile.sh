41d623a0436006b470f8be1ee721f4309febcde2
Enzo Lazzarini <enzolazzamor@gmail.com>
2025-04-15 13:34:22 -0300

    Refactoring packages

--- compile.sh
+++ compile.sh
@@ -7,13 +7,18 @@ generate_gRPC() {
 
 	GRPC_PACKAGE_DIR="${PACKAGES_DIR}/gRPC"
 	mkdir -p "${GRPC_PACKAGE_DIR}"
-	touch "${GRPC_PACKAGE_DIR}/__init__.py"
 
 	$PROTOC -I"${PROTO_DIR}" \
 		--python_out="${GRPC_PACKAGE_DIR}" \
 		--pyi_out="${GRPC_PACKAGE_DIR}" \
 		--grpc_python_out="${GRPC_PACKAGE_DIR}" \
 		"${PROTO_DIR}"/*.proto
+
+	protol \
+		--create-package --in-place \
+		--python-out "${GRPC_PACKAGE_DIR}" \
+		protoc --proto-path="${PROTO_DIR}" helloworld.proto kvs.proto \
+		--protoc-path "${PROTOC}"
 }
 
 install_requirements() {
@@ -28,28 +33,39 @@ build() {
 	if [ ! -d "${VENV_DIR}" ]; then
 		printf "\nCreating virtual environment...\n"
 		$PYTHON -m venv "${VENV_DIR}"
+
+		# printf "\nCreating symlink for protoc...\n"
+		# echo "${PROTOC} \"\$@\"" >"${VENV_DIR}/bin/protoc"
+		# chmod +x "${VENV_DIR}/bin/protoc"
+
+		source_venv
+
+		install_requirements
 	else
 		printf "\nVirtual environment already exists.\n"
 	fi
 
+	source_venv
+
 	mkdir -p "${PACKAGES_DIR}"
 	touch "${PACKAGES_DIR}/__init__.py"
 
-	source_venv
-
-	install_requirements
 	generate_gRPC
 }
 
 clean() {
 	printf "\nCleaning...\n"
-	# rm -rf "${VENV_DIR}"
+	rm -rf "${VENV_DIR}"
 	rm -rf "${PACKAGES_DIR}"
 }
 
 case $1 in
 "clean")
 	clean
+	exit
+	;;
+"requirements")
+	install_requirements
 	exit
 	;;
 "") ;;
